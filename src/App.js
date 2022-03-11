9ca229bf71ce7ba4b9d8a72929c116eadd52f1cb
Frida <fridaawesterdahl@gmail.com>
2022-03-11 18:17:03 +0100

    Update App.js
    FUNKAR!!!!!!!!!!!!1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import Start from './components/start/Start';
 import NewExpense from './components/newExpense/NewExpense';
-import Main from './components/main/Main';
-import Footer from './components/footer/Footer';
+import Main from './components/main/main';
+import Footer from './components/footer/footer';
 
 function App() {
   return (
