60d4c48e62b17154cf9a05167ff55bc96371386f
Frida <fridaawesterdahl@gmail.com>
2022-03-14 21:05:07 +0100

    More layout fixing and Expense-component

--- src/components/main/LatestExpense.js
+++ src/components/main/LatestExpense.js
@@ -1,45 +1,44 @@
 import './latestExpense.css';
 
+
+function Expense(props) {
+    return (
+        <li>
+            <p className='purchase-date' date={props.date}>{props.date}</p>
+            <p className='purchase-item' item={props.item}>{props.item}</p>
+            <p className='purchase-category' category={props.category}>{props.category}</p>
+            <p className='purchase-cost' cost={props.cost}>{props.cost}</p>
+            <button className='remove-purchase'>❌</button>
+        </li>
+    );
+}
+
 function LatestExpense() {
+
+    // const removeExpense = () =>
+
     return (
         <div id="expenses">
             <h3>L A T E S T &nbsp;&nbsp; E X P E N S E S</h3>
             <ul id="latest-expenses">
                 <li>
-                    <p id="purchase-date">01-03-2022</p>
-                    <p id="purchase-item">ICA</p>
-                    <p id="purchase-category">Food & Beverages</p>
-                    <p id="purchase-cost">300:-</p>
-                    <button id="remove-purchase">❌</button>
+                    <p className='purchase-date'>01-03-2022</p>
+                    <p className='purchase-item'>ICA</p>
+                    <p className='purchase-category'>Food &amp; Beverages</p>
+                    <p className='purchase-cost'>300:-</p>
+                    <button className='remove-purchase'>❌</button>
                 </li>
                 <li>
-                    <p id="purchase-date">01-03-2022</p>
-                    <p id="purchase-item">ICA</p>
-                    <p id="purchase-category">Food & Beverages</p>
-                    <p id="purchase-cost">300:-</p>
-                    <button id="remove-purchase">❌</button>
+                    <p className='purchase-date'>01-03-2022</p>
+                    <p className='purchase-item'>dfddgfgfd</p>
+                    <p className='purchase-category'>Electronics</p>
+                    <p className='purchase-cost'>3000:-</p>
+                    <button className='remove-purchase'>❌</button>
                 </li>
-                <li>
-                    <p id="purchase-date">01-03-2022</p>
-                    <p id="purchase-item">ICA</p>
-                    <p id="purchase-category">Food & Beverages</p>
-                    <p id="purchase-cost">300:-</p>
-                    <button id="remove-purchase">❌</button>
-                </li>
-                <li>
-                    <p id="purchase-date">01-03-2022</p>
-                    <p id="purchase-item">ICA</p>
-                    <p id="purchase-category">Food & Beverages</p>
-                    <p id="purchase-cost">300:-</p>
-                    <button id="remove-purchase">❌</button>
-                </li>
-                <li>
-                    <p id="purchase-date">01-03-2022</p>
-                    <p id="purchase-item">ICA</p>
-                    <p id="purchase-category">Food & Beverages</p>
-                    <p id="purchase-cost">300:-</p>
-                    <button id="remove-purchase">❌</button>
-                </li>
+                <Expense date="14-03-2022" item="Car" category="Other" cost="30000:-" />
+                <Expense date="14-03-2022" item="Shoes" category="Shopping" cost="800:-" />
+                <Expense date="14-03-2022" item="Clothes" category="Shopping" cost="500:-" />
+
             </ul>
             <div id="show-alternative">
                 <p>s h o w:</p>
--- src/components/start/Start.js
+++ src/components/start/Start.js
@@ -1,15 +1,20 @@
 import './start.css';
 import BigLogo from './BigLogo';
+import { useState } from 'react';
 
 function Money(props) {
     return (
-        <li id={props.id} name={props.name} money={props.money}>{props.name}: {props.money}</li>
+        <p id={props.id} name={props.name} money={props.money}>{props.name}: {props.money}</p>
     );
 }
 
 function Start() {
     const moneySpent = 290000;
     const moneyLeft = 20000;
+
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+
     return (
         <>
             {/* <header>
@@ -21,10 +26,13 @@ function Start() {
                 <img src="https://mb.cision.com/Public/977/3482646/aa5533be0d43e6fc_800x800ar.png"></img>
             </section>
             <div id="summary">
-                <p>Budget [SEK] </p><input id="budget" type="number"></input>
+                <p>Budget [SEK] <input id="budget" type="number"></input></p>
                 <Money id="money-spent" name="Money spent [SEK]" money={moneySpent} />
                 <Money id="money-left" name="Money left [SEK]" money={moneyLeft} />
             </div>
+            <button className={click ? 'details-button-clicked' : 'details-button'}
+                onClick={handleClick} type="button" value="text">
+                S H O W &ensp;&ensp; D E T A I L S</button>
             <BigLogo />
         </>
     );
