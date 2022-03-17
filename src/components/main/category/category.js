cb3fe07d73c9dd485c49d3c5fd1ca3deb25be2c9
Frida <fridaawesterdahl@gmail.com>
2022-03-17 16:18:00 +0100

    liveshare

--- src/components/main/category/category.js
+++ src/components/main/category/category.js
@@ -13,16 +13,25 @@ function Category() {
 
     const handleOptionChange = (event) => {
         event.preventDefault();
+        // this.useState(event.target.value)
         setOption(event.target.value);
+
+
+        console.log('event.target.value ' + event.target.value)
+        console.log('option: ' + option)
     }
+    const value = setOption;
+
+
+
 
     return (
         <>
-            <select id="category-options" name="category" value={option} onChange={handleOptionChange}>
-                <Options id="category" class="options" name="Choose category" value={option} />
-                <Options id="food" name="Food" value={option} />
-                <Options id="shopping" name="Shopping" value={option} />
-                <Options id="electronics" name="Electronics" value={option} />
+            <select id="category-options" name="category" value={setOption} onChange={handleOptionChange}>
+                <Options id="category" class="options" name="Choose category" />
+                <Options id="food" name="Food" />
+                <Options id="shopping" name="Shopping" />
+                <Options id="electronics" name="Electronics" value="Electronics" />
             </select>
         </>
     );
