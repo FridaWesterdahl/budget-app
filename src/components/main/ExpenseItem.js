c7d5927128e4e98c50cbd4152eef3ccb1e7fedb1
Fredrik Lam <fredrik.didi.lam@gmail.com>
2022-03-27 20:21:16 +0200

    Completed functionality
    Renamed file name
    Fixed Filter to search

--- src/components/main/ExpenseItem.js
+++ src/components/main/ExpenseItem.js
@@ -1,27 +1,18 @@
 import React from "react";
 
 export default function ExpenseItem({ expenses, index, removeExpense}) {
-    // let date = new Date(expenses.date);
-    // let day = date.getDate();
-    // let month = date.getMonth() +1;
-    // let year = date.getFullYear();
 
     const handleRemove = (i) => {
-        removeExpense(i);
-        // console.log('moonth: ' + month)        
+        removeExpense(i);      
     }
 
     return (
         <li className="listItem">
-            {/* <p className="purchase-date">{year + "-" + month + "-" + day}</p> */}
             <p className="purchase-date">{expenses.date}</p>
             <p className="purchase-item">{expenses.item}</p>
             <p className="purchase-category">{expenses.category}</p>
             <p className="purchase-cost">{expenses.cost}:-</p>
             <button className="remove-purchase" onClick={() => handleRemove(index)}>❌</button>
         </li>
-
     );
 }
-
-// {year + "-" + month + "-" + day}
